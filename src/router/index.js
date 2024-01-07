2d2bb0825bc0b93fd1e672f508b452bbd11446ae
Adi Nugraha Putra <nugrahaadi733@gmail.com>
2024-01-07 10:57:07 +0700

    router/update import

--- src/router/index.js
+++ src/router/index.js
@@ -4,8 +4,8 @@ import HomeView from "@/views/HomeView.vue"
 import CategoriesView from "@/views/CategoriesView.vue"
 import PricingView from "@/views/PricingView.vue"
 import StudyCaseView from "@/views/StudyCaseView.vue"
-import LoginView from "../views/LoginView.vue"
-import RegisterView from "../views/RegisterView.vue"
+import LoginView from "@/views/LoginView.vue"
+import RegisterView from "@/views/RegisterView.vue"
 
 const router = createRouter({
    history: createWebHistory(import.meta.env.BASE_URL),

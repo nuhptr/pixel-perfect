a9fdbb858f91d5d6221c6f86c1484e9635674fc5
Adi Nugraha Putra <nugrahaadi733@gmail.com>
2024-01-07 14:08:45 +0700

    router/update code

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router"
 
 import HomeView from "@/views/HomeView.vue"
+import ProductDetailView from "@/views/ProductDetailView.vue"
 import CategoriesView from "@/views/CategoriesView.vue"
 import PricingView from "@/views/PricingView.vue"
 import StudyCaseView from "@/views/StudyCaseView.vue"
@@ -10,12 +11,42 @@ import RegisterView from "@/views/RegisterView.vue"
 const router = createRouter({
    history: createWebHistory(import.meta.env.BASE_URL),
    routes: [
-      { path: "/", name: "home", component: HomeView },
-      { path: "/categories", name: "categories", component: CategoriesView },
-      { path: "/pricing", name: "pricing", component: PricingView },
-      { path: "/study-case", name: "study-case", component: StudyCaseView },
-      { path: "/login", name: "login", component: LoginView },
-      { path: "/register", name: "register", component: RegisterView },
+      {
+         path: "/",
+         name: "home",
+         component: HomeView,
+      },
+      {
+         // path: "/details/:id",
+         path: "/product-details",
+         name: "product-details",
+         component: ProductDetailView,
+      },
+      {
+         path: "/categories",
+         name: "categories",
+         component: CategoriesView,
+      },
+      {
+         path: "/pricing",
+         name: "pricing",
+         component: PricingView,
+      },
+      {
+         path: "/study-case",
+         name: "study-case",
+         component: StudyCaseView,
+      },
+      {
+         path: "/login",
+         name: "login",
+         component: LoginView,
+      },
+      {
+         path: "/register",
+         name: "register",
+         component: RegisterView,
+      },
       // {
       //    path: "/about",
       //    name: "about",

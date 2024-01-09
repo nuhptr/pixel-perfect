c8668cbf38d1931684c59542c96e41890b78e4fb
nuhptr <nugrahaadi733@gmail.com>
2024-01-09 22:03:01 +0700

    update category title

--- src/components/categories/DetailItems.vue
+++ src/components/categories/DetailItems.vue
@@ -6,11 +6,10 @@
    import ItemsCard from "../ItemsCard.vue"
 
    let items = ref([])
+   let category = ref({})
    let isLoading = ref(false)
 
    const route = useRoute()
-
-   defineProps({ titleSection: String })
 
    async function getItemsData() {
       try {
@@ -21,6 +20,7 @@
          // console.log(response.data.data.products)
 
          items.value = response.data.data.products
+         category.value = response.data.data
          isLoading.value = false
       } catch (error) {
          console.log(error)
@@ -36,9 +36,7 @@
 <template>
    <!-- Item -->
    <div class="container px-4 mx-auto my-16 md:px-12">
-      <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">
-         {{ titleSection ?? "New Items" }}
-      </h2>
+      <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">{{ category.name }}</h2>
       <div class="flex flex-wrap -mx-1 lg:-mx-4">
          <div v-if="isLoading" class="flex items-center justify-center w-full">Isloading...</div>
          <ItemsCard v-else v-for="item in items" :key="item.id" :id="item.id" :item="item" />
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -7,10 +7,12 @@
       <div class="mx-auto max-w-7xl">
          <div class="flex flex-col items-center w-full min-h-screen pt-6 sm:justify-center sm:pt-0">
             <div class="w-full p-5 mx-auto sm:max-w-md">
-               <h2 class="mb-20 text-5xl font-bold text-center">Welcome Back</h2>
+               <h2 class="mb-20 text-5xl font-bold leading-tight tracking-tight text-center">
+                  Hi, let's check new design kit
+               </h2>
                <LoginForm />
             </div>
-            <div class="w-full mx-auto mt-10 sm:max-w-2xl">
+            <div class="w-full mx-auto mt-12 sm:max-w-2xl">
                <img src="@/assets/img/brand-logo.svg" alt="" class="w-full px-10 md:px-0" />
             </div>
          </div>

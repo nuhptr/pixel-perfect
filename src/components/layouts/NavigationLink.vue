81f5c0aa9091e091eeb21152b48bba6f3a6c16ab
nuhptr <nugrahaadi733@gmail.com>
2024-01-13 15:52:11 +0700

    dynamic style when active

--- src/components/layouts/NavigationLink.vue
+++ src/components/layouts/NavigationLink.vue
@@ -1,31 +1,35 @@
 <script setup>
 import { RouterLink } from "vue-router"
+import { ref } from "vue"
+
+let isActive = ref('')
+
 </script>
 
 <template>
    <div class="items-center justify-between hidden w-full md:pl-2 lg:flex md:w-auto md:order-1" id="mobile-menu-2">
       <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-regular">
          <li>
-            <RouterLink to="/"
-               class="block py-2 pl-3 pr-4 text-white bg-indigo-600 rounded md:bg-transparent md:text-indigo-600 md:p-0"
+            <RouterLink to="/" active-class="bg-indigo-600 rounded md:bg-transparent md:text-indigo-600"
+               class="block py-2 pl-3 pr-4 text-gray-800 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-indigo-600 md:p-0"
                aria-current="page">
                Home
             </RouterLink>
          </li>
          <li>
-            <RouterLink to="/categories"
+            <RouterLink to="/categories" active-class="bg-indigo-600 rounded md:bg-transparent md:text-indigo-600"
                class="block py-2 pl-3 pr-4 text-gray-800 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-indigo-600 md:p-0">
                Categories
             </RouterLink>
          </li>
          <li>
-            <RouterLink to="/pricing"
+            <RouterLink to="/pricing" active-class="bg-indigo-600 rounded md:bg-transparent md:text-indigo-600"
                class="block py-2 pl-3 pr-4 text-gray-800 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-indigo-600 md:p-0">
                Pricing
             </RouterLink>
          </li>
          <li>
-            <RouterLink to="/study-case"
+            <RouterLink to="/study-case" active-class="bg-indigo-600 rounded md:bg-transparent md:text-indigo-600"
                class="block py-2 pl-3 pr-4 text-gray-800 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-indigo-600 md:p-0">
                Study Case
             </RouterLink>
